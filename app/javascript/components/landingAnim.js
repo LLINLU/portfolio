46672e62c8447952d82bc94a0b928fc03777bc07
linlu liu <linlu96@yahoo.com>
2020-11-18 17:29:19 +0900

    nav

--- app/javascript/components/landingAnim.js
+++ app/javascript/components/landingAnim.js
@@ -1,7 +1,8 @@
 
 const landingAnim = () => {
   const hero = document.querySelector(".girl_img");
-  const text = document.querySelector('.about')
+  const text = document.querySelector('.about');
+  const nav =document.querySelectorAll('.nav-link');
   if(hero){
   const tl = new  TimelineMax();
   tl.fromTo(
@@ -10,6 +11,7 @@ const landingAnim = () => {
     {height: "0%"},
     {height: "100%", ease: Power2.easeInOut}
   )
+
   .fromTo(
     hero,
     5.2,
@@ -17,8 +19,16 @@ const landingAnim = () => {
     {width: '660px', ease: Power2.easeInOut },
     "-=1.2"
   )
+
   .fromTo(text, 4.5, {opacity: 0, x: 30}, {opacity: 1, x: 0},
   "-=0.5"
+  )
+    .fromTo(
+     nav,
+     1,
+     {height: "0%", opacity: 0},
+     {height: "100%", ease: Power2.easeInOut, opacity:1},
+     "-=3.5"
   );
 }}
 
