a3ea12c3a83564cb4cb072d9560aa0ebf483b9b6
linlu liu <linlu96@yahoo.com>
2020-11-19 23:58:51 +0900

    done

--- app/javascript/components/horizontalSlide.js
+++ app/javascript/components/horizontalSlide.js
@@ -4,7 +4,7 @@ const horizontalSlide = () => {
   var controller = new ScrollMagic.Controller();
   var horizontalSlide = new TimelineMax()
   // animate panels
-  .to("#js-slideContainer", 1,   {x: "-20%"})
+  .to("#js-slideContainer", 3,   {x: "-20%"})
   .to("#js-slideContainer", 1,   {x: "-40%"})
   .to("#js-slideContainer", 1,   {x: "-60%"})
   .to("#js-slideContainer", 1,   {x: "-80%"})
