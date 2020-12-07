70f855578f60cc88e3e1ada9927cda66162b4ab9
linlu liu <linlu96@yahoo.com>
2020-12-08 00:04:56 +0900

    done

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
 
   get '/services', to: 'pages#services'
   get '/works', to: 'works#index'
+  get '/works/cookpal', to: 'works#cookpal'
 
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
