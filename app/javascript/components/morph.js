f02c99e27ebc39e0fc9f5b72d47c7103d9ab4ada
linlu liu <linlu96@yahoo.com>
2021-01-07 06:44:55 +0900

    morph

--- app/javascript/components/morph.js
+++ app/javascript/components/morph.js
@@ -4,7 +4,7 @@ const morphBlobs = () => {
 let tween = KUTE.fromTo('#blob3', {path: '#blob3' }, { path: '#blob2' }, // to shape
    { // options
       easing: 'easingCubicInOut',
-      yoyo: true, repeat: 100, duration: 2500}).start();
+      yoyo: true, repeat: 1100, duration: 2500 }).start();
 console.log(tween)
 // or same path into a square
 // var tween2 = KUTE.to('#blob1', { path: '#blob3' }).start();
